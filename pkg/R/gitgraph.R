c77715b028ebed84e5fda096829d14c381beedc3
timelyportfolio <kent.russell@timelyportfolio.com>
2016-08-23 10:51:23 -0500

    make a package and add favorite starter pieces, scaffoldWidget

---
+++ pkg/R/gitgraph.R
@@ -0,0 +1,52 @@
+#' <Add Title>
+#'
+#' <Add Description>
+#'
+#' @import htmlwidgets
+#'
+#' @export
+gitgraph <- function(message, width = NULL, height = NULL, elementId = NULL) {
+
+  # forward options using x
+  x = list(
+    message = message
+  )
+
+  # create widget
+  htmlwidgets::createWidget(
+    name = 'gitgraph',
+    x,
+    width = width,
+    height = height,
+    package = 'pkg',
+    elementId = elementId
+  )
+}
+
+#' Shiny bindings for gitgraph
+#'
+#' Output and render functions for using gitgraph within Shiny
+#' applications and interactive Rmd documents.
+#'
+#' @param outputId output variable to read from
+#' @param width,height Must be a valid CSS unit (like \code{'100\%'},
+#'   \code{'400px'}, \code{'auto'}) or a number, which will be coerced to a
+#'   string and have \code{'px'} appended.
+#' @param expr An expression that generates a gitgraph
+#' @param env The environment in which to evaluate \code{expr}.
+#' @param quoted Is \code{expr} a quoted expression (with \code{quote()})? This
+#'   is useful if you want to save an expression in a variable.
+#'
+#' @name gitgraph-shiny
+#'
+#' @export
+gitgraphOutput <- function(outputId, width = '100%', height = '400px'){
+  htmlwidgets::shinyWidgetOutput(outputId, 'gitgraph', width, height, package = 'pkg')
+}
+
+#' @rdname gitgraph-shiny
+#' @export
+renderGitgraph <- function(expr, env = parent.frame(), quoted = FALSE) {
+  if (!quoted) { expr <- substitute(expr) } # force quoted
+  htmlwidgets::shinyRenderWidget(expr, gitgraphOutput, env, quoted = TRUE)
+}
---
+++ pkg/README.md
@@ -0,0 +1,7 @@
+
+<!-- README.md is generated from README.Rmd. Please edit that file -->
+[![Travis-CI Build Status](https://travis-ci.org/.svg?branch=master)](https://travis-ci.org/) [![CRAN\_Status\_Badge](http://www.r-pkg.org/badges/version/pkg)](https://cran.r-project.org/package=pkg)
+
+### Code of Conduct
+
+Please note that this project is released with a [Contributor Code of Conduct](CONDUCT.md). By participating in this project you agree to abide by its terms.
